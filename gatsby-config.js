833b320f34816bb2690e7d3d10429f044f699957
Francisco <fran.perez.c@hotmail.com>
2020-07-03 03:19:56 -0300

    Netlify configuration 2.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-identity-widget',
       options: {
-        container: '/admin/'
+        container: 'head'
       }
     },
     {
