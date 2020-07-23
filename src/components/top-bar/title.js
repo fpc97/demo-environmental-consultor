964b6e886fb5cc9d19a114e4562374a3b50a9673
Francisco <fran.perez.c@hotmail.com>
2020-07-23 19:22:36 -0300

    Titles ordered

--- src/components/top-bar/title.js
+++ src/components/top-bar/title.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default () => (
-    <hgroup className="title">
+export default ({bottomBar = false}) => (
+    <hgroup className={`title${bottomBar ? ' title--bottom-bar' : ''}`}>
         <h1 className="title__h1">Evelina Cejuela <small>y Asociados</small></h1>
         <span className="title__subtitle">Servicios de Consultoria Ambiental</span>
     </hgroup>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,23 @@ import Title from '../components/top-bar/title';
 export default () => (
     <Layout noTitle>
         <SEO title="Principal"/>
-
         {/* BACKGROUND */}
+        <div className="home-background">
+            <div className="home-background__slideshow">&nbsp;</div>
+            <div className="home-background__blue">
+                <div className="home-background__gradient"></div>
+                <div className="home-background__plain"></div>
+            </div>
+            <div className="home-background__svg">&nbsp;</div>
+        </div>
 
         <header className="header">
-            <Title/>
+            <Title bottomBar/>
             <h2 className="header__h2"><span className="header__h2--first">BIOLOGIA</span> <br/><span className="header__h2--last">DIGITAL</span></h2>
-            <p className="header__intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-            <button className="header__main-button">Saber Más →</button>
+            <div className="header__bottom-flex">
+                <p className="header__intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                <button className="header__main-button">Saber Más →</button>
+            </div>
         </header>
     </Layout>
 );
