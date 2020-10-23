fc2df518f415471b92e95c35e7f2be0f35e36e9a
Francisco <fran.perez.c@hotmail.com>
2020-10-23 11:53:41 -0300

    Change image fetch URL from static to dynamic in schema/locks

--- src/templates/schema.js
+++ src/templates/schema.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default () => (
   <div>
-    <img src="/img/default-avatar.jpg"/> 
-    <img src="/img/default-bat.jpg"/>
-    <img src="/img/favicon.png"/>
-    <img src="/img/non-display.png"/>
+    <img src="../img/default-avatar.jpg"/> 
+    <img src="../img/default-bat.jpg"/>
+    <img src="../img/favicon.png"/>
+    <img src="../img/non-display.png"/>
   </div>
 )
