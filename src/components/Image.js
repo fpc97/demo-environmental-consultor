91e3e4001aa47b2c697d67716863e678874adc17
Francisco <fran.perez.c@hotmail.com>
2022-04-05 01:51:38 -0300

    update gatsby-plugin-image import

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { GatsbyImage as Img } from "gatsby-plugin-image"
 
 const Image = () => {
   const data = useStaticQuery(graphql`
--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Img from 'gatsby-image'
+import { GatsbyImage as Img } from "gatsby-plugin-image"
 
 const PreviewCompatibleImage = ({
   imageInfo,
